c7b4954a79dd554b84329fe24f41593707d5f949
Mahendra Kumar <zzsdrt354@gmail.com>
2026-03-21 16:50:21 +0530

    Fix: Default path clear command bug fixed.
    Signed-off-by: Mahendra Kumar <zzsdrt354@gmail.com>

--- src/Conditional_Argument/commands/Clear/Clear.cpp
+++ src/Conditional_Argument/commands/Clear/Clear.cpp
@@ -20,6 +20,7 @@ int Clear(int argc, char* argv[])
 		std::string error = "Path does not exist! [Clear/Reset arg]";
 		if(!condtnl_arg_cls.path_exists(base_path, error)) return -1;
 
+		// If file is empty at base_path + bool.txt than return -1
 		int running = condtnl_arg_cls.read_status(base_path);
 		std::cout << running << std::endl;
 		if(running == -1) return -1;
@@ -28,9 +29,8 @@ int Clear(int argc, char* argv[])
 	{
 		// Default path mode ----------------------------------
 		base_path = condtnl_arg_cls.get_default_path();
-
 		// Checking whether default path is not set than return -1
-		if(base_path == "...");
+		if(base_path == "...")
 		{
 			std::cout << "Set the default path! [Clear/Reset arg]\n";
 			return -1;
