79cca6b0aa93039f77878211af70d03f60760283
Mahendra Kumar <zzsdrt354@gmail.com>
2026-04-03 13:25:48 +0530

    Refactor: Default_Path_Utility.
    ~ Added safety check to prevent undefined program behavior.
    ~ Added explanatory comments.
    
    Signed-off-by: Mahendra Kumar <zzsdrt354@gmail.com>

--- src/Utility/Default_Path_Utility/Default_Path.cpp
+++ src/Utility/Default_Path_Utility/Default_Path.cpp
@@ -1,17 +1,34 @@
 #include "./Default_Path.hpp"
 #include "../Trim_Spaces_Utility/Trim_spaces.hpp"
+#include "../../../src/Conditional_Argument/commands/Default/Default.hpp"
 
 #include <string>
-
+#include <iostream>
 DEFAULT_PATH_UTILITY_CLASS default_path_utility;
 DEFAULT_PATH_UTILITY_CLASS::DEFAULT_PATH_UTILITY_CLASS(){};
 
+// Grab the default path in file content and white spaces trimed ------------------------
 std::string DEFAULT_PATH_UTILITY_CLASS::default_path(const std::string& file_content)
 {
-	size_t start = file_content.find("=") + 1, end = file_content.find("-");
+	size_t start = file_content.find("=") + 1, end = file_content.find("-"),
+	       eqaul = file_content.find("=") - 1;
 
 	// Grab default path --------------------------------------------------
 	std::string default_path = file_content.substr(start, end - start);
 
+	/*
+	 * Check if the directory file actually contains a valid "default_path" entry.
+	 *
+	 * If this entry is missing, it likely means the user manually deleted or
+	 * modified the default path in the directory file.
+	 *
+	 * Without this validation, the program may incorrectly treat a user path
+	 * as the default path, leading to an invalid architecture state and
+	 * potential crash. This check prevents that condition.
+	 */
+	if(file_content.substr(0, eqaul) != "default_path") return "...";
+
+	// Trim white spaces --------------------------------------------------
 	return trim_spaces_class.trim_spaces(default_path);
 }
+// --------------------------------------------------------------------------------------
